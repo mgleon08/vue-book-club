c0831674ed0cca4ee67c5b7247736328ff0a9e90
Leon Ji <mgleon08@gmail.com>
2018-06-21 18:08:54 +0800

    Modify eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,6 +24,12 @@ module.exports = {
     // allow async-await
     'generator-star-spacing': 'off',
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'space-before-function-paren': 0,
+    'new-cap': [
+      'error', {
+        'properties': false
+      }
+    ]
   }
 }
